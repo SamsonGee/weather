9802a3478aec105c2bc9fc31f6f33fcc682a8c9c
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-10-12 12:57:09 -0500

    copied over alt code and will not rework

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -33,7 +33,7 @@ var getCityName = function(city) {
           response.json()
           .then(function(data) {
           console.log(data);
-          displayRepos(data, city);
+          displayCityInfo(data, city);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -43,6 +43,45 @@ var getCityName = function(city) {
       // Notice this `.catch()` getting chained onto the end of the `.then()` method
       alert("Unable to connect to capture city");
     });
+  };
+
+  var displayCityInfo = function(repos, citySearch) {
+   
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  
+    // create a container for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+  
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+  
+    // append to container
+    repoEl.appendChild(titleEl);
+    
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+    statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+    }
   };
 
 cityFormEl.addEventListener("submit", formSubmitHandler);
